e4443f2ad8c933bb301e84ed3663f0bf0efdc2b7
阿菌•未霜 <799620521@qq.com>
2022-03-20 21:26:09 +0800

    zram: Do not subtract deduplicated data from compressed size when freeing pages
    In the function __zram_bvec_write, when we match that the checksum of the
    current data is already in the list, we will skip the compression operation
    and allocate the zram entry directly, and will not calculate the size of the
    duplicate data in the storage page compression size .
    
    In the function zram_free_page, the deduplicated data size is directly
    subtracted from the stored page compression size indiscriminately, which will
    inevitably lead to statistical errors, and compr_data_size will continue to
    decrease, so that it will be less than 0 after a period of time. The data is
    written into the structure of u64, causing the data to exceed the value.
    
    Therefore, we add a flag to record which pages have been deduplicated, and no
    longer subtract the size of these pages from compr_data_size when freeing pages.
    
    Change-Id: Idedf4b7ddd5e2f6afb5dde8a2f8c55c4b1aec481
    Signed-off-by: pix106 <sbordenave@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1204,6 +1204,7 @@ static bool zram_meta_alloc(struct zram *zram, u64 disksize)
 static void zram_free_page(struct zram *zram, size_t index)
 {
 	struct zram_entry *entry;
+	bool is_deduped;
 
 #ifdef CONFIG_ZRAM_MEMORY_TRACKING
 	zram->table[index].ac_time = 0;
@@ -1211,7 +1212,8 @@ static void zram_free_page(struct zram *zram, size_t index)
 	if (zram_test_flag(zram, index, ZRAM_IDLE))
 		zram_clear_flag(zram, index, ZRAM_IDLE);
 
-	if (zram_test_flag(zram, index, ZRAM_DEDUPED))
+	is_deduped = zram_test_flag(zram, index, ZRAM_DEDUPED);
+	if (is_deduped)
 		zram_clear_flag(zram, index, ZRAM_DEDUPED);
 
 	if (zram_test_flag(zram, index, ZRAM_HUGE)) {
@@ -1241,8 +1243,9 @@ static void zram_free_page(struct zram *zram, size_t index)
 
 	zram_entry_free(zram, entry);
 
-	atomic64_sub(zram_get_obj_size(zram, index),
-			&zram->stats.compr_data_size);
+	if (!is_deduped)
+		atomic64_sub(zram_get_obj_size(zram, index),
+			     &zram->stats.compr_data_size);
 out:
 	atomic64_dec(&zram->stats.pages_stored);
 	zram_set_entry(zram, index, NULL);
