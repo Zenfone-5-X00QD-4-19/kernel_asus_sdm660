59b54fb713adde24903f1bd779271e5884356fba
henricaodopao1 <dragonsxbr@gmail.com>
2023-07-25 19:07:29 +0000

    drivers: input: touchscreen: import FT8716 touchscreen driver
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- drivers/input/touchscreen/focaltech_touch/focaltech_config.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_config.h
@@ -2,7 +2,7 @@
  *
  * FocalTech TouchScreen driver.
  *
- * Copyright (c) 2012-2019, FocalTech Systems, Ltd., all rights reserved.
+ * Copyright (c) 2010-2016, FocalTech Systems, Ltd., all rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -14,18 +14,17 @@
  * GNU General Public License for more details.
  *
  */
-
 /************************************************************************
 *
 * File Name: focaltech_config.h
 *
-* Author: Focaltech Driver Team
+*    Author: Focaltech Driver Team
 *
-* Created: 2016-08-08
+*   Created: 2016-08-08
 *
-* Abstract: global configurations
+*  Abstract: global configurations
 *
-* Version: v1.0
+*   Version: v1.0
 *
 ************************************************************************/
 #ifndef _LINUX_FOCLATECH_CONFIG_H_
@@ -34,104 +33,35 @@
 /**************************************************/
 /****** G: A, I: B, S: C, U: D  ******************/
 /****** chip type defines, do not modify *********/
-#define _FT8716             0x87160805
-#define _FT8736             0x87360806
-#define _FT8006M            0x80060807
-#define _FT8607             0x86070809
-#define _FT8006U            0x8006D80B
-#define _FT8006S            0x8006A80B
-#define _FT8613             0x8613080C
-#define _FT8719             0x8719080D
-#define _FT8739             0x8739080E
-#define _FT8615             0x8615080F
-#define _FT8201             0x82010810
-#define _FT8006P            0x86220811
-#define _FT7251             0x72510812
-#define _FT7252             0x72520813
-#define _FT8613S            0x8613C814
-#define _FT8756             0x87560815
-#define _FT8302             0x83020816
-#define _FT8009             0x80090817
-#define _FT8656             0x86560818
-#define _FT8006S_AA         0x86320819
-#define _FT7250             0x7250081A
-
-
-#define _FT5416             0x54160402
-#define _FT5426             0x54260402
-#define _FT5435             0x54350402
-#define _FT5436             0x54360402
-#define _FT5526             0x55260402
-#define _FT5526I            0x5526B402
-#define _FT5446             0x54460402
-#define _FT5346             0x53460402
-#define _FT5446I            0x5446B402
-#define _FT5346I            0x5346B402
-#define _FT7661             0x76610402
-#define _FT7511             0x75110402
-#define _FT7421             0x74210402
-#define _FT7681             0x76810402
-#define _FT3C47U            0x3C47D402
-#define _FT3417             0x34170402
-#define _FT3517             0x35170402
-#define _FT3327             0x33270402
-#define _FT3427             0x34270402
-#define _FT7311             0x73110402
-
-#define _FT5626             0x56260401
-#define _FT5726             0x57260401
-#define _FT5826B            0x5826B401
-#define _FT5826S            0x5826C401
-#define _FT7811             0x78110401
-#define _FT3D47             0x3D470401
-#define _FT3617             0x36170401
-#define _FT3717             0x37170401
-#define _FT3817B            0x3817B401
-#define _FT3517U            0x3517D401
-
-#define _FT6236U            0x6236D003
-#define _FT6336G            0x6336A003
-#define _FT6336U            0x6336D003
-#define _FT6436U            0x6436D003
-
-#define _FT3267             0x32670004
-#define _FT3367             0x33670004
-
-#define _FT3327DQQ_XXX      0x3327D482
-#define _FT5446DQS_XXX      0x5446D482
-
-#define _FT3518             0x35180481
-#define _FT3558             0x35580481
-#define _FT3528             0x35280481
-#define _FT5536             0x55360481
-
-#define _FT5446U            0x5446D083
-#define _FT5456U            0x5456D083
-#define _FT3417U            0x3417D083
-#define _FT5426U            0x5426D083
-#define _FT3428             0x34280083
-#define _FT3437U            0x3437D083
-
-#define _FT7302             0x73020084
-#define _FT7202             0x72020084
-#define _FT3308             0x33080084
-
-#define _FT6346U            0x6346D085
-#define _FT6346G            0x6346A085
-#define _FT3067             0x30670085
-#define _FT3068             0x30680085
-#define _FT3168             0x31680085
-#define _FT3268             0x32680085
+#define _FT8716     0x8716
+#define _FT8736     0x8736
+#define _FT8607     0x8607
+#define _FT8006     0x8006
+#define _FT5436     0x5436
+#define _FT5446     0x5446
+#define _FT5346     0x5346
+#define _FT5446I    0x5446B
+#define _FT5346I    0x5346B
+#define _FT3327     0x3327
+#define _FT3427     0x3427
+#define _FT6236U    0x6236D
+#define _FT6336G    0x6336A
+#define _FT6336U    0x6336D
+#define _FT6436U    0x6436D
+#define _FT5826S    0x5836C
+#define _FT5626     0x5626
+#define _FT5726     0x5726
 
 /*************************************************/
 
 /*
  * choose your ic chip type of focaltech
  */
-#define FTS_CHIP_TYPE   _FT3518
+#define FTS_CHIP_TYPE   _FT8716
 
 /******************* Enables *********************/
 /*********** 1 to enable, 0 to disable ***********/
+//#define FTS_INIT_WORK                  0
 
 /*
  * show debug log info
@@ -149,13 +79,21 @@
  * Report Pressure in multitouch
  * 1:enable(default),0:disable
 */
-#define FTS_REPORT_PRESSURE_EN                  1
+#define FTS_REPORT_PRESSURE_EN                  0
+
+/*
+ * Force touch support
+ * different pressure for multitouch
+ * 1: true pressure for force touch
+ * 0: constant pressure(default)
+ */
+#define FTS_FORCE_TOUCH_EN                      0
 
 /*
  * Gesture function enable
  * default: disable
  */
-#define FTS_GESTURE_EN                          0
+#define FTS_GESTURE_EN                          1
 
 /*
  * ESD check & protection
@@ -163,80 +101,141 @@
  */
 #define FTS_ESDCHECK_EN                         0
 
+/*
+ * Production test enable
+ * 1: enable, 0:disable(default)
+ */
+#define FTS_TEST_EN                             1
 
 /*
- * Pinctrl enable
+ * Glove mode enable
+ * 1: enable, 0:disable(default)
+ */
+#define FTS_GLOVE_EN                            1
+/*
+ * cover enable
+ * 1: enable, 0:disable(default)
+ */
+#define FTS_COVER_EN                            1
+/*
+ * Charger enable
+ * 1: enable, 0:disable(default)
+ */
+#define FTS_CHARGER_EN                          1
+
+/*
+ * Proximity sensor
  * default: disable
  */
-#define FTS_PINCTRL_EN                          1
+#define FTS_PSENSOR_EN                          0
+
+/*
+ * Nodes for tools, please keep enable
+ */
+#define FTS_SYSFS_NODE_EN                       1
+#define FTS_APK_NODE_EN                         1
 
 /*
  * Customer power enable
  * enable it when customer need control TP power
  * default: disable
  */
-#define FTS_POWER_SOURCE_CUST_EN                1
+#define FTS_POWER_SOURCE_CUST_EN                0
 
 /****************************************************/
 
+/*
+ * max touch points number
+ * default: 10
+ */
+#define FTS_MAX_POINTS                          10
+
 /********************** Upgrade ****************************/
 /*
- * auto upgrade
+ * auto upgrade, please keep enable
  */
 #define FTS_AUTO_UPGRADE_EN                     1
 
 /*
  * auto upgrade for lcd cfg
+ * default: 0
  */
-#define FTS_AUTO_LIC_UPGRADE_EN                 0
+#define FTS_AUTO_UPGRADE_FOR_LCD_CFG_EN         0
+
+/* auto cb check
+ * default: disable
+ */
+#define FTS_AUTO_CLB_EN                         0
 
 /*
- * Numbers of modules support
+ * FW_APP.i file for upgrade
+ * define your own fw_app, the sample one is invalid
  */
-#define FTS_GET_MODULE_NUM                      0
+#define FTS_UPGRADE_FW_APP                      "include/firmware/ASUS_TITAN_V0x67_app.i"
+#define FTS_UPGRADE_FW_APP_ARA                  "include/firmware/ASUS_ARA_8716F_03_V0x65_20181004_app.i"
+/*
+ * auto upgrade with app.bin in sdcard
+ */
+#define FTS_UPGRADE_WITH_APP_BIN_EN             0
 
 /*
- * module_id: mean vendor_id generally, also maybe gpio or lcm_id...
- * If means vendor_id, the FTS_MODULE_ID = PANEL_ID << 8 + VENDOR_ID
- * FTS_GET_MODULE_NUM == 0/1, no check module id, you may ignore them
- * FTS_GET_MODULE_NUM >= 2, compatible with FTS_MODULE2_ID
- * FTS_GET_MODULE_NUM >= 3, compatible with FTS_MODULE3_ID
+ * app.bin files for upgrade with app bin file
+ * this file must put in sdcard
  */
-#define FTS_MODULE_ID                          0x0000
-#define FTS_MODULE2_ID                         0x0000
-#define FTS_MODULE3_ID                         0x0000
+#define FTS_UPGRADE_FW_APP_BIN                  "app.bin"
 
 /*
- * Need set the following when get firmware via firmware_request()
- * For example: if module'vendor is tianma,
- * #define FTS_MODULE_NAME                        "tianma"
- * then file_name will be "focaltech_ts_fw_tianma"
- * You should rename fw to "focaltech_ts_fw_tianma", and push it into
- * etc/firmware or by customers
+ * lcd_cfg.i file for lcd cfg upgrade
+ * define your own lcd_cfg.i, the sample one is invalid
  */
-#define FTS_MODULE_NAME                        "gvo"
-#define FTS_MODULE2_NAME                       ""
-#define FTS_MODULE3_NAME                       ""
+//Note: Not used! Please Ignore it!
+//#define FTS_UPGRADE_LCD_CFG                     "include/firmware/lcd_cfg.i"
 
 /*
- * FW.i file for auto upgrade, you must replace it with your own
- * define your own fw_file, the sample one to be replaced is invalid
- * NOTE: if FTS_GET_MODULE_NUM > 1, it's the fw corresponding with FTS_VENDOR_ID
+ * auto upgrade with app.bin in sdcard
  */
-#define FTS_UPGRADE_FW_FILE                    "include/firmware/fw_sample.i"
+#define FTS_UPGRADE_LCD_CFG_BIN_EN              0
 
 /*
- * if FTS_GET_MODULE_NUM >= 2, fw corrsponding with FTS_VENDOR_ID2
- * define your own fw_file, the sample one is invalid
+ * lcd_cfg.bin files for upgrade with lcd cfg bin file
+ * this file must put in sdcard
  */
-#define FTS_UPGRADE_FW2_FILE                   "include/firmware/fw_sample.i"
+#define FTS_UPGRADE_LCD_CFG_BIN                 "lcd_cfg.bin"
 
 /*
- * if FTS_GET_MODULE_NUM >= 3, fw corrsponding with FTS_VENDOR_ID3
- * define your own fw_file, the sample one is invalid
+ * vendor_id(s) for the ic
+ * you need confirm vendor_id for upgrade
+ * if only one vendor, ignore vendor_2_id, otherwise
+ * you need define both of them
  */
-#define FTS_UPGRADE_FW3_FILE                   "include/firmware/fw_sample.i"
+#define FTS_VENDOR_1_ID                         0x01
+#define FTS_VENDOR_2_ID                         0x8d
+
+/* show upgrade time in log
+ * default: disable
+ */
+#define FTS_GET_UPGRADE_TIME                    0
+
+/* get vedor id from flash
+ * default: enable
+ */
+#define FTS_GET_VENDOR_ID_FROM_FLASH            1
+
+/* get project id from flash
+ * default: disable
+ */
+#define FTS_GET_PROJECT_ID_FROM_FLASH           0
+
+/*
+ * upgrade ping-pong test for debug
+ * enable it for upgrade debug if needed
+ * default: disable
+ */
+#define FTS_UPGRADE_PINGPONG_TEST               0
+/* pingpong or error test times, default: 1000 */
+#define FTS_UPGRADE_TEST_NUMBER                 1000
 
 /*********************************************************/
 
 #endif /* _LINUX_FOCLATECH_CONFIG_H_ */
+
--- drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
+++ drivers/input/touchscreen/focaltech_touch/focaltech_i2c.c
@@ -2,7 +2,7 @@
  *
  * FocalTech TouchScreen driver.
  *
- * Copyright (c) 2012-2019, FocalTech Systems, Ltd., all rights reserved.
+ * Copyright (c) 2010-2016, FocalTech Systems, Ltd., all rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -19,16 +19,17 @@
 *
 * File Name: focaltech_i2c.c
 *
-* Author: Focaltech Driver Team
+*    Author: fupeipei
 *
-* Created: 2016-08-04
+*   Created: 2016-08-04
 *
-* Abstract: i2c communication with TP
+*  Abstract: i2c communication with TP
 *
-* Version: v1.0
+*   Version: v1.0
 *
 * Revision History:
-*
+*        v1.0:
+*            First release. By fupeipei 2016-08-04
 ************************************************************************/
 
 /*****************************************************************************
@@ -39,8 +40,6 @@
 /*****************************************************************************
 * Private constant and macro definitions using #define
 *****************************************************************************/
-#define I2C_RETRY_NUMBER                    3
-#define I2C_BUF_LENGTH                      256
 
 /*****************************************************************************
 * Private enumerations, structures and unions using typedef
@@ -49,6 +48,7 @@
 /*****************************************************************************
 * Static variables
 *****************************************************************************/
+static DEFINE_MUTEX(i2c_rw_access);
 
 /*****************************************************************************
 * Global variable or extern global variabls/functions
@@ -61,133 +61,155 @@
 /*****************************************************************************
 * functions body
 *****************************************************************************/
-int fts_read(u8 *cmd, u32 cmdlen, u8 *data, u32 datalen)
+
+/************************************************************************
+* Name: fts_i2c_read
+* Brief: i2c read
+* Input: i2c info, write buf, write len, read buf, read len
+* Output: get data in the 3rd buf
+* Return: fail <0
+***********************************************************************/
+int fts_i2c_read(struct i2c_client *client, char *writebuf,int writelen, char *readbuf, int readlen)
 {
-	int ret = 0;
-	int i = 0;
-	struct fts_ts_data *ts_data = fts_data;
-	struct i2c_msg msg_list[2];
-	struct i2c_msg *msg = NULL;
-	int msg_num = 0;
+    int ret;
 
-	/* must have data when read */
-	if (!ts_data || !ts_data->client || !data || !datalen
-		|| (datalen >= I2C_BUF_LENGTH) || (cmdlen >= I2C_BUF_LENGTH)) {
-		FTS_ERROR("fts_data/client/cmdlen(%d)/data/datalen(%d) is invalid",
-			cmdlen, datalen);
-		return -EINVAL;
-	}
+    mutex_lock(&i2c_rw_access);
 
-	mutex_lock(&ts_data->bus_lock);
-	memset(&msg_list[0], 0, sizeof(struct i2c_msg));
-	memset(&msg_list[1], 0, sizeof(struct i2c_msg));
-	memcpy(ts_data->bus_tx_buf, cmd, cmdlen);
-	msg_list[0].addr = ts_data->client->addr;
-	msg_list[0].flags = 0;
-	msg_list[0].len = cmdlen;
-	msg_list[0].buf = ts_data->bus_tx_buf;
-	msg_list[1].addr = ts_data->client->addr;
-	msg_list[1].flags = I2C_M_RD;
-	msg_list[1].len = datalen;
-	msg_list[1].buf = ts_data->bus_rx_buf;
-	if (cmd && cmdlen) {
-		msg = &msg_list[0];
-		msg_num = 2;
-	} else {
-		msg = &msg_list[1];
-		msg_num = 1;
-	}
+    if (readlen > 0)
+    {
+        if (writelen > 0)
+        {
+            struct i2c_msg msgs[] =
+            {
+                {
+                    .addr = client->addr,
+                    .flags = 0,
+                    .len = writelen,
+                    .buf = writebuf,
+                },
+                {
+                    .addr = client->addr,
+                    .flags = I2C_M_RD,
+                    .len = readlen,
+                    .buf = readbuf,
+                },
+            };
+            ret = i2c_transfer(client->adapter, msgs, 2);
+            if (ret < 0)
+            {
+                FTS_ERROR("[IIC]: i2c_transfer(write) error, ret=%d!!", ret);
+            }
+        }
+        else
+        {
+            struct i2c_msg msgs[] =
+            {
+                {
+                    .addr = client->addr,
+                    .flags = I2C_M_RD,
+                    .len = readlen,
+                    .buf = readbuf,
+                },
+            };
+            ret = i2c_transfer(client->adapter, msgs, 1);
+            if (ret < 0)
+            {
+                FTS_ERROR("[IIC]: i2c_transfer(read) error, ret=%d!!", ret);
+            }
+        }
+    }
 
-	for (i = 0; i < I2C_RETRY_NUMBER; i++) {
-		ret = i2c_transfer(ts_data->client->adapter, msg, msg_num);
-		if (ret < 0) {
-			FTS_ERROR("i2c_transfer(read) fail,ret:%d", ret);
-		} else {
-			memcpy(data, ts_data->bus_rx_buf, datalen);
-			break;
-		}
-	}
-
-	mutex_unlock(&ts_data->bus_lock);
-	return ret;
+    mutex_unlock(&i2c_rw_access);
+    return ret;
 }
 
-int fts_write(u8 *writebuf, u32 writelen)
+/************************************************************************
+* Name: fts_i2c_write
+* Brief: i2c write
+* Input: i2c info, write buf, write len
+* Output: no
+* Return: fail <0
+***********************************************************************/
+int fts_i2c_write(struct i2c_client *client, char *writebuf, int writelen)
 {
-	int ret = 0;
-	int i = 0;
-	struct fts_ts_data *ts_data = fts_data;
-	struct i2c_msg msgs;
+    int ret = 0;
 
-	if (!ts_data || !ts_data->client || !writebuf || !writelen
-		|| (writelen >= I2C_BUF_LENGTH)) {
-		FTS_ERROR("fts_data/client/data/datalen(%d) is invalid", writelen);
-		return -EINVAL;
-	}
+    mutex_lock(&i2c_rw_access);
+    if (writelen > 0)
+    {
+        struct i2c_msg msgs[] =
+        {
+            {
+                .addr = client->addr,
+                .flags = 0,
+                .len = writelen,
+                .buf = writebuf,
+            },
+        };
+        ret = i2c_transfer(client->adapter, msgs, 1);
+        if (ret < 0)
+        {
+            FTS_ERROR("%s: i2c_transfer(write) error, ret=%d", __func__, ret);
+        }
+    }
+    mutex_unlock(&i2c_rw_access);
 
-	mutex_lock(&ts_data->bus_lock);
-	memset(&msgs, 0, sizeof(struct i2c_msg));
-	memcpy(ts_data->bus_tx_buf, writebuf, writelen);
-	msgs.addr = ts_data->client->addr;
-	msgs.flags = 0;
-	msgs.len = writelen;
-	msgs.buf = ts_data->bus_tx_buf;
-	for (i = 0; i < I2C_RETRY_NUMBER; i++) {
-		ret = i2c_transfer(ts_data->client->adapter, &msgs, 1);
-		if (ret < 0) {
-			FTS_ERROR("i2c_transfer(write) fail,ret:%d", ret);
-		} else {
-			break;
-		}
-	}
-	mutex_unlock(&ts_data->bus_lock);
-	return ret;
+    return ret;
 }
 
-int fts_read_reg(u8 addr, u8 *value)
+/************************************************************************
+* Name: fts_i2c_write_reg
+* Brief: write register
+* Input: i2c info, reg address, reg value
+* Output: no
+* Return: fail <0
+***********************************************************************/
+int fts_i2c_write_reg(struct i2c_client *client, u8 regaddr, u8 regvalue)
 {
-	return fts_read(&addr, 1, value, 1);
+    u8 buf[2] = {0};
+
+    buf[0] = regaddr;
+    buf[1] = regvalue;
+    return fts_i2c_write(client, buf, sizeof(buf));
+}
+/************************************************************************
+* Name: fts_i2c_read_reg
+* Brief: read register
+* Input: i2c info, reg address, reg value
+* Output: get reg value
+* Return: fail <0
+***********************************************************************/
+int fts_i2c_read_reg(struct i2c_client *client, u8 regaddr, u8 *regvalue)
+{
+    return fts_i2c_read(client, &regaddr, 1, regvalue, 1);
 }
 
-int fts_write_reg(u8 addr, u8 value)
+/************************************************************************
+* Name: fts_i2c_init
+* Brief: fts i2c init
+* Input:
+* Output:
+* Return:
+***********************************************************************/
+int fts_i2c_init(void)
 {
-	u8 buf[2] = { 0 };
+    FTS_FUNC_ENTER();
 
-	buf[0] = addr;
-	buf[1] = value;
-	return fts_write(buf, sizeof(buf));
+    FTS_FUNC_EXIT();
+    return 0;
+}
+/************************************************************************
+* Name: fts_i2c_exit
+* Brief: fts i2c exit
+* Input:
+* Output:
+* Return:
+***********************************************************************/
+int fts_i2c_exit(void)
+{
+    FTS_FUNC_ENTER();
+
+    FTS_FUNC_EXIT();
+    return 0;
 }
 
-int fts_bus_init(struct fts_ts_data *ts_data)
-{
-	FTS_FUNC_ENTER();
-	ts_data->bus_tx_buf = kzalloc(I2C_BUF_LENGTH, GFP_KERNEL);
-	if (NULL == ts_data->bus_tx_buf) {
-		FTS_ERROR("failed to allocate memory for bus_tx_buf");
-		return -ENOMEM;
-	}
-
-	ts_data->bus_rx_buf = kzalloc(I2C_BUF_LENGTH, GFP_KERNEL);
-	if (NULL == ts_data->bus_rx_buf) {
-		FTS_ERROR("failed to allocate memory for bus_rx_buf");
-		return -ENOMEM;
-	}
-	FTS_FUNC_EXIT();
-	return 0;
-}
-
-int fts_bus_exit(struct fts_ts_data *ts_data)
-{
-	FTS_FUNC_ENTER();
-	if (ts_data && ts_data->bus_tx_buf) {
-		kfree(ts_data->bus_tx_buf);
-		ts_data->bus_tx_buf = NULL;
-	}
-
-	if (ts_data && ts_data->bus_rx_buf) {
-		kfree(ts_data->bus_rx_buf);
-		ts_data->bus_rx_buf = NULL;
-	}
-	FTS_FUNC_EXIT();
-	return 0;
-}
