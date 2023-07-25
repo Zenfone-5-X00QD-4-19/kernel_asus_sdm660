39422052bebbd02afea20327f580c88289c04b7c
henricaodopao1 <dragonsxbr@gmail.com>
2023-07-25 19:07:29 +0000

    drivers: input: touchscreen: import FT8716 touchscreen driver
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- drivers/input/touchscreen/focaltech_touch/focaltech_common.h
+++ drivers/input/touchscreen/focaltech_touch/focaltech_common.h
@@ -2,7 +2,7 @@
  *
  * FocalTech fts TouchScreen driver.
  *
- * Copyright (c) 2012-2019, Focaltech Ltd. All rights reserved.
+ * Copyright (c) 2010-2016, Focaltech Ltd. All rights reserved.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -36,133 +36,161 @@
 /*****************************************************************************
 * Macro definitions using #define
 *****************************************************************************/
-#define FTS_DRIVER_VERSION      "Focaltech V3.1 20190807"
+#define FTS_DRIVER_VERSION                  "Focaltech V1.1 20161103"
 
-#define BYTE_OFF_0(x)           (u8)((x) & 0xFF)
-#define BYTE_OFF_8(x)           (u8)(((x) >> 8) & 0xFF)
-#define BYTE_OFF_16(x)          (u8)(((x) >> 16) & 0xFF)
-#define BYTE_OFF_24(x)          (u8)(((x) >> 24) & 0xFF)
-#define FLAGBIT(x)              (0x00000001 << (x))
-#define FLAGBITS(x, y)          ((0xFFFFFFFF >> (32 - (y) - 1)) & (0xFFFFFFFF << (x)))
+/* pramboot */
+#define FTS_PRAMBOOT_8716   "include/pramboot/FT8716_Pramboot_V0.5_20160723.i"
+#define FTS_UPGRADE_PRAMBOOT    FTS_PRAMBOOT_8716
 
-#define FLAG_ICSERIALS_LEN      8
-#define FLAG_HID_BIT            10
-#define FLAG_IDC_BIT            11
 
-#define IC_SERIALS              (FTS_CHIP_TYPE & FLAGBITS(0, FLAG_ICSERIALS_LEN-1))
-#define IC_TO_SERIALS(x)        ((x) & FLAGBITS(0, FLAG_ICSERIALS_LEN-1))
-#define FTS_CHIP_IDC            ((FTS_CHIP_TYPE & FLAGBIT(FLAG_IDC_BIT)) == FLAGBIT(FLAG_IDC_BIT))
-#define FTS_HID_SUPPORTTED      ((FTS_CHIP_TYPE & FLAGBIT(FLAG_HID_BIT)) == FLAGBIT(FLAG_HID_BIT))
 
-#define FTS_CHIP_TYPE_MAPPING {{0x81, 0x54, 0x52, 0x54, 0x52, 0x00, 0x00, 0x54, 0x5C}}
+#define FTS_CHIP_IDC            1
 
+#define FTS_CHIP_TYPE_MAPPING {_FT8716, 0x87, 0x16, 0x87, 0x16, 0x87, 0xA6, 0x00, 0x00}
+
+#define I2C_BUFFER_LENGTH_MAXINUM           256
 #define FILE_NAME_LENGTH                    128
 #define ENABLE                              1
 #define DISABLE                             0
-#define VALID                               1
-#define INVALID                             0
-#define FTS_CMD_START1                      0x55
-#define FTS_CMD_START2                      0xAA
-#define FTS_CMD_START_DELAY                 12
-#define FTS_CMD_READ_ID                     0x90
-#define FTS_CMD_READ_ID_LEN                 4
-#define FTS_CMD_READ_ID_LEN_INCELL          1
 /*register address*/
 #define FTS_REG_INT_CNT                     0x8F
 #define FTS_REG_FLOW_WORK_CNT               0x91
 #define FTS_REG_WORKMODE                    0x00
 #define FTS_REG_WORKMODE_FACTORY_VALUE      0x40
 #define FTS_REG_WORKMODE_WORK_VALUE         0x00
-#define FTS_REG_ESDCHECK_DISABLE            0x8D
 #define FTS_REG_CHIP_ID                     0xA3
-#define FTS_REG_CHIP_ID2                    0x9F
 #define FTS_REG_POWER_MODE                  0xA5
-#define FTS_REG_POWER_MODE_SLEEP            0x03
+#define FTS_REG_POWER_MODE_SLEEP_VALUE      0x03
 #define FTS_REG_FW_VER                      0xA6
 #define FTS_REG_VENDOR_ID                   0xA8
 #define FTS_REG_LCD_BUSY_NUM                0xAB
 #define FTS_REG_FACE_DEC_MODE_EN            0xB0
-#define FTS_REG_FACTORY_MODE_DETACH_FLAG    0xB4
-#define FTS_REG_FACE_DEC_MODE_STATUS        0x01
-#define FTS_REG_IDE_PARA_VER_ID             0xB5
-#define FTS_REG_IDE_PARA_STATUS             0xB6
 #define FTS_REG_GLOVE_MODE_EN               0xC0
 #define FTS_REG_COVER_MODE_EN               0xC1
-#define FTS_REG_REPORT_RATE                 0x88
 #define FTS_REG_CHARGER_MODE_EN             0x8B
 #define FTS_REG_GESTURE_EN                  0xD0
 #define FTS_REG_GESTURE_OUTPUT_ADDRESS      0xD3
-#define FTS_REG_MODULE_ID                   0xE3
-#define FTS_REG_LIC_VER                     0xE4
 #define FTS_REG_ESD_SATURATE                0xED
 
-#define FTS_SYSFS_ECHO_ON(buf)      (buf[0] == '1')
-#define FTS_SYSFS_ECHO_OFF(buf)     (buf[0] == '0')
-
-#define kfree_safe(pbuf) do {\
-	if (pbuf) {\
-		kfree(pbuf);\
-		pbuf = NULL;\
-	}\
-} while(0)
-
-/*****************************************************************************
-*  Alternative mode (When something goes wrong,
-* the modules may be able to solve the problem.)
-*****************************************************************************/
-/*
- * point report check
- * default: disable
- */
-#define FTS_POINT_REPORT_CHECK_EN               0
 
 /*****************************************************************************
 * Global variable or extern global variabls/functions
 *****************************************************************************/
-struct ft_chip_t {
-	u64 type;
-	u8 chip_idh;
-	u8 chip_idl;
-	u8 rom_idh;
-	u8 rom_idl;
-	u8 pb_idh;
-	u8 pb_idl;
-	u8 bl_idh;
-	u8 bl_idl;
+struct ft_chip_t
+{
+    unsigned long type;
+    unsigned char chip_idh;
+    unsigned char chip_idl;
+    unsigned char rom_idh;
+    unsigned char rom_idl;
+    unsigned char pramboot_idh;
+    unsigned char pramboot_idl;
+    unsigned char bootloader_idh;
+    unsigned char bootloader_idl;
 };
 
-struct ts_ic_info {
-	bool is_incell;
-	bool hid_supported;
-	struct ft_chip_t ids;
+struct fts_Upgrade_Fun
+{
+    unsigned char chip_idh;
+    unsigned char chip_idl;
+    bool app_valid;
+    bool need_upgrade;
+    bool get_projectid;
+    unsigned char project_id[0x20];
+    int (*get_app_bin_file_ver)(char *);
+    int (*get_app_i_file_ver)(void);
+    int (*upgrade_with_app_i_file)(struct i2c_client *);
+    int (*upgrade_with_app_bin_file)(struct i2c_client *, char *);
+    int (*upgrade_with_lcd_cfg_i_file)(struct i2c_client *);
+    int (*upgrade_with_lcd_cfg_bin_file)(struct i2c_client *, char *);
 };
+extern struct fts_Upgrade_Fun fts_updatefun;
+
+/* i2c communication*/
+int fts_i2c_write_reg(struct i2c_client *client, u8 regaddr, u8 regvalue);
+int fts_i2c_read_reg(struct i2c_client *client, u8 regaddr, u8 *regvalue);
+int fts_i2c_read(struct i2c_client *client, char *writebuf,int writelen, char *readbuf, int readlen);
+int fts_i2c_write(struct i2c_client *client, char *writebuf, int writelen);
+int fts_i2c_init(void);
+int fts_i2c_exit(void);
+
+/* Gesture functions */
+#if FTS_GESTURE_EN
+int fts_gesture_init(struct input_dev *input_dev, struct i2c_client *client);
+int fts_gesture_exit(struct i2c_client *client);
+void fts_gesture_recovery(struct i2c_client *client);
+int fts_gesture_readdata(struct i2c_client *client);
+int fts_gesture_suspend(struct i2c_client *i2c_client);
+int fts_gesture_resume(struct i2c_client *client);
+#endif
+
+/* Apk and functions */
+#if FTS_APK_NODE_EN
+int fts_create_apk_debug_channel(struct i2c_client * client);
+void fts_release_apk_debug_channel(void);
+#endif
+
+/* ADB functions */
+#if FTS_SYSFS_NODE_EN
+int fts_create_sysfs(struct i2c_client *client);
+int fts_remove_sysfs(struct i2c_client *client);
+#endif
+
+/* ESD */
+#if FTS_ESDCHECK_EN
+int fts_esdcheck_init(void);
+int fts_esdcheck_exit(void);
+int fts_esdcheck_switch(bool enable);
+int fts_esdcheck_proc_busy(bool proc_debug);
+int fts_esdcheck_set_intr(bool intr);
+int fts_esdcheck_suspend(void);
+int fts_esdcheck_resume(void);
+int fts_esdcheck_get_status(void);
+#endif
+
+/* Production test */
+#if FTS_TEST_EN
+int fts_test_init(struct i2c_client *client);
+int fts_test_exit(struct i2c_client *client);
+#endif
+
+/* Other */
+extern int g_show_log;
+int fts_reset_proc(int hdelayms);
+int fts_wait_tp_to_valid(struct i2c_client *client);
+void fts_tp_state_recovery(struct i2c_client *client);
+int fts_ex_mode_init(struct i2c_client *client);
+int fts_ex_mode_exit(struct i2c_client *client);
+int fts_ex_mode_recovery(struct i2c_client *client);
 
 /*****************************************************************************
 * DEBUG function define here
 *****************************************************************************/
 #if FTS_DEBUG_EN
-#define FTS_DEBUG(fmt, args...) do { \
-	printk("[FTS_TS]%s:"fmt"\n", __func__, ##args); \
-} while (0)
+#define FTS_DEBUG_LEVEL     2
 
-#define FTS_FUNC_ENTER() do { \
-	printk("[FTS_TS]%s: Enter\n", __func__); \
-} while (0)
+#if (FTS_DEBUG_LEVEL == 1)
+#define FTS_DEBUG(fmt, args...) printk(KERN_ERR "[FTS][%s]"fmt"\n", __func__, ##args)
+#else
+#define FTS_DEBUG(fmt, args...) printk(KERN_ERR "[FTS]"fmt"\n", ##args)
+#endif
 
-#define FTS_FUNC_EXIT() do { \
-	printk("[FTS_TS]%s: Exit(%d)\n", __func__, __LINE__); \
-} while (0)
-#else /* #if FTS_DEBUG_EN*/
+#define FTS_FUNC_ENTER() printk(KERN_ERR "[FTS]%s: Enter\n", __func__)
+#define FTS_FUNC_EXIT()  printk(KERN_ERR "[FTS]%s: Exit(%d)\n", __func__, __LINE__)
+#else
 #define FTS_DEBUG(fmt, args...)
 #define FTS_FUNC_ENTER()
 #define FTS_FUNC_EXIT()
 #endif
 
 #define FTS_INFO(fmt, args...) do { \
-	printk(KERN_INFO "[FTS_TS/I]%s:"fmt"\n", __func__, ##args); \
-} while (0)
+            if (g_show_log) {printk(KERN_ERR "[FTS][Info]"fmt"\n", ##args);} \
+        }  while (0)
 
-#define FTS_ERROR(fmt, args...) do { \
-	printk(KERN_ERR "[FTS_TS/E]%s:"fmt"\n", __func__, ##args); \
-} while (0)
+#define FTS_ERROR(fmt, args...)  do { \
+             if (g_show_log) {printk(KERN_ERR "[FTS][Error]"fmt"\n", ##args);} \
+        }  while (0)
+
+
 #endif /* __LINUX_FOCALTECH_COMMON_H__ */
+
