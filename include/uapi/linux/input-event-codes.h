b5cec51e62ccd3d1af66b4341ead90fe94f3959a
henricaodopao1 <dragonsxbr@gmail.com>
2023-08-11 20:37:37 +0000

    include: uapi: linux: import goodix event codes
    Signed-off-by: henricaodopao1 <dragonsxbr@gmail.com>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -28,6 +28,14 @@
 #define INPUT_PROP_POINTING_STICK	0x05	/* is a pointing stick */
 #define INPUT_PROP_ACCELEROMETER	0x06	/* has accelerometer */
 
+#define FP_KEY_UP		0x258
+#define FP_KEY_DOWN		0x259
+#define FP_KEY_LEFT		0x25a
+#define FP_KEY_RIGHT		0x25b
+#define FP_KEY_CLICK		0x25c
+#define FP_KEY_DOUBLE_CLICK	0x25d
+#define FP_KEY_LONG_PRESS	0x25e
+
 #define INPUT_PROP_MAX			0x1f
 #define INPUT_PROP_CNT			(INPUT_PROP_MAX + 1)
 
